ac063747f2f3cfb4edb65910da7cd76734a6516e
JaimeBea <jaimebeapz@gmail.com>
2020-09-16 13:47:50 +0200

    Initial configuration

---
+++ common.h
@@ -0,0 +1,8 @@
+#ifndef JAIMEBEA_ZORK_COMMON_H__
+#define JAIMEBEA_ZORK_COMMON_H__
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+#endif // !JAIMEBEA_ZORK_COMMON_H__
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include "common.h"
+
+int main() {
+	std::cout << "Hello world!\n\n";
+
+	system("pause");
+	return 0;
+}
